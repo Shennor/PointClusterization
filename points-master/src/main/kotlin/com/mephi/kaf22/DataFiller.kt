5015e0af2cb553d45448b0113868fc8ff4960b04
Aleksandrov Nikita <nikitosiks338@gmail.com>
2023-01-15 16:12:07 +0300

    More data

--- points-master/src/main/kotlin/com/mephi/kaf22/DataFiller.kt
+++ points-master/src/main/kotlin/com/mephi/kaf22/DataFiller.kt
@@ -7,11 +7,11 @@ import java.io.File
 object DataFiller {
     @JvmStatic
     fun main(args: Array<String>) {
-        val file = File("./data/testData2.txt")
+        val file = File("./data/testData3.txt")
         val writer = file.writer()
         val random = kotlin.random.Random(42)
         writer.appendLine("7")
-        (1..99999).map {
+        (1..999).map {
             Point2(
                 random.nextDouble(-10000.0, 10000.0),
                 random.nextDouble(-10000.0, 10000.0),
